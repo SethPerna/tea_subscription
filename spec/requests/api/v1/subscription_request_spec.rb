56d20509125d058d145b4ff8fb5e42c49d186372
Seth Perna <gseth26@gmail.com>
2022-04-13 10:16:45 -0400

    finish 2nd endpoint for subscription update and tea_sub destroy. Controller needs major refactoring and security mesaures. Happy path testing complete

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -37,5 +37,34 @@ RSpec.describe 'subscription request' do
     headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
     post '/api/v1/subscriptions', headers: headers, params: JSON.generate(data)
     expect(response.status).to eq(404)
-  en
+  end
+
+  it "cancels a sunscription(patch)" do
+    customer = Customer.create!(first_name: "Seth", last_name: "Perna", email: "gseth26@gmail.com", address: "123 Sesame St")
+    tea = Tea.create!(title: "Chamomile", description: "Sleepy Time", temp: 175, brewtime: "00:10:00")
+    subscription = customer.subscriptions.create!(title: "#{customer.first_name}'s #{tea.title} subscription", price: 10.0, status: 0, frequency: 0)
+    tea_sub = TeaSub.create!(tea_id: tea.id, subscription_id: subscription.id)
+    data = { email: customer.email,
+             tea_id: tea.id,
+             subscription_id: subscription.id
+     }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    patch '/api/v1/subscriptions', headers: headers, params: JSON.generate(data)
+    subscription = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(subscription).to have_key(:data)
+    expect(subscription[:data]).to have_key(:id)
+    expect(subscription[:data]).to have_key(:type)
+    expect(subscription[:data][:type]).to eq("subscription")
+    expect(subscription[:data]).to have_key(:attributes)
+    expect(subscription[:data][:attributes]).to have_key(:title)
+    expect(subscription[:data][:attributes]).to have_key(:price)
+    expect(subscription[:data][:attributes]).to have_key(:status)
+    expect(subscription[:data][:attributes]).to have_key(:frequency)
+    expect(subscription[:data][:attributes][:title]).to eq("Seth's Chamomile subscription")
+    expect(subscription[:data][:attributes][:price]).to eq(10.0)
+    expect(subscription[:data][:attributes][:status]).to eq("inactive")
+    expect(subscription[:data][:attributes][:frequency]).to eq("weekly")
+  end
 end
