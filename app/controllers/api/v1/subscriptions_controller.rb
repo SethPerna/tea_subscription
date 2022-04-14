2c7d1cef98f6d22ba5f2d2d52309e6f418a4c6f4
Seth Perna <gseth26@gmail.com>
2022-04-13 20:26:58 -0400

    refactor controller into private actions, to dry up code

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::SubscriptionsController < ApplicationController
-
+  before_action :find_data
   def index
-    customer = Customer.find_by(email: params[:email])
-    if customer != nil
-      render json: CustomerSubscriptionSerializer.new(customer.subscriptions)
+    if @customer != nil
+      render json: CustomerSubscriptionSerializer.new(@customer.subscriptions)
     else
       render status: 404
     end
@@ -11,10 +10,8 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def create
     if params[:email].present?
-      customer = Customer.find_by(email: params[:email])
-      tea = Tea.find_by(title: params[:tea_title])
-      subscription = Subscription.create(title: "#{customer.first_name}'s Tea subscription", price: 10.0, frequency: params[:frequency], customer_id: customer.id)
-      tea_sub = TeaSub.create(tea_id: tea.id, subscription_id: subscription.id)
+      subscription = @customer.subscriptions.create(title: "#{@customer.first_name}'s Tea subscription", price: 10.0, frequency: params[:frequency], customer_id: @customer.id)
+      tea_sub = TeaSub.create(tea_id: @tea.id, subscription_id: subscription.id)
       if subscription.save
         render json: SubscriptionSerializer.new(subscription), status: 201
       else
@@ -25,9 +22,7 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def update
     if params[:subscription_id].present?
-      customer = Customer.find_by(email: params[:email])
       subscription = Subscription.find(params[:subscription_id])
-      tea = Tea.find(params[:tea_id])
       subscription.update(status: 1)
       tea_sub = TeaSub.find_by(tea_id: params[:tea_id], subscription_id: params[:subscription_id])
       tea_sub.destroy
@@ -38,5 +33,12 @@ class Api::V1::SubscriptionsController < ApplicationController
         render status: 404
       end
     end
+  end
+
+  private
+
+  def find_data
+    @customer = Customer.find_by(email: params[:email])
+    @tea = Tea.find_by(title: params[:tea_title])
   end
 end
